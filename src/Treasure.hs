51cbfb4e1ad0ede8b4b54157e42f8ed1e21779c4
Patrik Maunus <subscription@skriptladan.se>
2020-07-23 20:33:34 +0200

    Change signature for reseetsAt

--- src/Treasure.hs
+++ src/Treasure.hs
@@ -12,7 +12,7 @@ module Treasure (
 import Data.Time
 import Treasure.Model
 
-resetsAt :: TreasureLog -> UTCTime
-resetsAt (TreasureLog _ time _) =
+resetsAt :: UTCTime -> UTCTime
+resetsAt time =
     let resetTime = addDays 30 $ utctDay time
     in UTCTime resetTime $ utctDayTime time
