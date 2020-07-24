a49bb656a769709f33bf01006b60a01474f36440
Patrik Maunus <subscription@skriptladan.se>
2020-07-24 07:00:14 +0200

    Print logs nicely to console

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.ByteString.Lazy.UTF8 (fromString)
+import qualified Data.Vector as Vector
+import Data.List (unlines)
+import qualified Data.Text.Lazy as LT
+
+import Data.Time
+
+import Formatting
+import Formatting.Formatters
+
 import Treasure
+import Treasure.Csv
 
 main :: IO ()
-main = return ()
+main = do
+    contents <- getContents
+    putStr (present contents)
+
+present :: String -> String
+present = either id prettyPrint . readCsv . fromString
+
+prettyPrint :: Vector.Vector TreasureLog -> String
+prettyPrint = unlines . map presentLog . Vector.toList
+
+presentLog :: TreasureLog -> String
+presentLog (TreasureLog name time location) =
+    formatToString (right 25 ' ' % right 45 ' ' % shown)
+        (unPlayerName name) time location
